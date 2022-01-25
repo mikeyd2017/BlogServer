48cfb0a874d2dad7db59e996bc849224e03edd91
Michael D'Ambrosio <dambrosiomichael@rocketmail.com>
2022-01-24 23:43:14 -0600

    Mobile CSS and Apps Icon

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { Button } from '@mui/material';
 import SignupModal from './SignupModal';
+import { IoLayers } from 'react-icons/io5';
 import '../Styles/sidebar.scss';
 
 function Sidebar(props) {
@@ -14,6 +14,13 @@ function Sidebar(props) {
                 <div className='user-info'>
                     <img src="https://via.placeholder.com/200x200" />
                     <p>{props.user.name}</p>
+                </div>
+                <div className='middle'>
+
+                </div>
+                <div className='bottom'>
+                    <IoLayers></IoLayers>
+                    <span>Apps</span>
                 </div>
                 {/* <Button onClick={() => setShowSignup(!showSignup)}>Sign Up</Button> */}
             </div>
