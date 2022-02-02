91fa0620837fe4a83b98ed63f00f02d9135458b7
Michael D'Ambrosio <dambrosiomichael@rocketmail.com>
2022-02-01 21:24:57 -0600

    App Design

--- client/src/Components/Main.jsx
+++ client/src/Components/Main.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react';
 import Blogs from './Blogs';
 
 import '../Styles/main.scss';
-import Sidebar from './Sidebar';
 import USER from '../utilities/User';
 
 function Main() {
@@ -14,7 +13,7 @@ function Main() {
     });
 
     useEffect(() => {
-        USER.getUser('61e711cb9139f4b16c893b0b').then(data => {
+        USER.getUser('61e0d941486e029487f69c72').then(data => {
             setUserState((userState) => ({
                 ...userState,
                 name: data.user.name,
@@ -27,7 +26,6 @@ function Main() {
     return (
         <div className='main-container'>
             <div className='main'>
-                <Sidebar user={userState}></Sidebar>
                 <Blogs name={userState.name}></Blogs>
             </div>
         </div>
