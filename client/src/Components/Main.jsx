9a5b074d861cf4d4b1c7450ddcacc2e48ea398c4
Michael D'Ambrosio <dambrosiomichael@rocketmail.com>
2022-02-22 18:04:27 -0600

    Background Fixes

--- client/src/Components/Main.jsx
+++ client/src/Components/Main.jsx
@@ -19,7 +19,7 @@ function Main() {
     });
 
     useEffect(() => {
-        USER.getUser('61e711cb9139f4b16c893b0b').then(data => {
+        USER.getUser('61e0d941486e029487f69c72').then(data => {
             setUserState((userState) => ({
                 ...userState,
                 name: data.user.name,
@@ -31,10 +31,11 @@ function Main() {
 
     return (
         <div className='main-container'>
-            <div className='bg'></div>
-            <div className='bg bg2'></div>
-            <div className='bg bg3'></div>
             <div className='main'>
+                <div className='bg'></div>
+                <div className='bg bg2'></div>
+                <div className='bg bg3'></div>
+                <div className='main-background'></div>
                 <Blogs name={userState.name}></Blogs>
             </div>
         </div>
